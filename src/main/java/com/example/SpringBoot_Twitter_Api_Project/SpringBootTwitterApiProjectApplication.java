31d5749c5d8a453a121c5aff127054528c677979
Burak-Salca <salcaburak@gmail.com>
2025-02-03 20:07:55 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringBoot_Twitter_Api_Project/SpringBootTwitterApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Twitter_Api_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwitterApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwitterApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_Twitter_Api_Project/SpringBootTwitterApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Twitter_Api_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTwitterApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
