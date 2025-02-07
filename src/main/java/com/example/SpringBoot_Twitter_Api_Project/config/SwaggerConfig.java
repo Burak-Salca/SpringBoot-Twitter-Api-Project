37bdfec23f1b71c9311b0d3a0f1d6f137c9109f3
Burak-Salca <salcaburak@gmail.com>
2025-02-07 05:54:03 +0300

    düzenlendi

--- src/main/java/com/example/SpringBoot_Twitter_Api_Project/config/SwaggerConfig.java
+++ src/main/java/com/example/SpringBoot_Twitter_Api_Project/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.SpringBoot_Twitter_Api_Project.controller"))
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.ant("/user/register"))
                 .build();
 
     }
